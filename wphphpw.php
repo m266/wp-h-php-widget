d3587b1a2c08918781e1b0752b1fd953b4210a52
m266 <hans.herbrand@gmail.com>
2017-08-15 23:59:49 +0200

    Update 1.0

--- wphphpw.php
+++ wphphpw.php
@@ -2,10 +2,10 @@
 
 /*
 Plugin Name: WP H-PHP-Widget
-Plugin URI: https://github.com/m266/php-widget
+Plugin URI: https://github.com/m266/wp-h-php-widget
 Description: Dieses Plugin f&uuml;gt ein Widget zum Einf&uuml;gen von PHP-Code im Dashboard hinzu.
 Version: 1.0
-Date: 2017-08-15
+Date: 2017-08-16
 Text Domain: php-widget
 Author: Hans M. Herbrand
 Author URI: http://www.web266.de
@@ -18,14 +18,14 @@ License URI: http://www.gnu.org/licenses/gpl-2.0.html
 defined('ABSPATH') || exit();
 // Plugin-Updater
 require 'plugin-update-checker/plugin-update-checker.php';
-$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker('https://github.com/m266/php-widget', __FILE__, 'php-widget');
+$myUpdateChecker = Puc_v4_Factory::buildUpdateChecker('https://github.com/m266/wp-h-php-widget', __FILE__, 'wp-h-php-widget');
 class PHP_Widget extends WP_Widget {
 // Frontend-Design Funktionen
   public function __construct() {
-    load_plugin_textdomain('php-widget', false, dirname(plugin_basename(__FILE__)));
-    $widget_ops = array('classname' => 'widget_execphp', 'description' => __('Beliebiger PHP-Code. ', 'php-widget'));
+    load_plugin_textdomain('wp-h-php-widget', false, dirname(plugin_basename(__FILE__)));
+    $widget_ops = array('classname' => 'widget_execphp', 'description' => __('Beliebiger PHP-Code. ', 'wp-h-php-widget'));
     $control_ops = array('width' => 400, 'height' => 350);
-    parent::__construct('execphp', __('PHP', 'php-widget'), $widget_ops, $control_ops);
+    parent::__construct('execphp', __('PHP', 'wp-h-php-widget'), $widget_ops, $control_ops);
   }
   public function widget($args, $instance) {
 // Funktionen für die Ausgabe des Codes auf der Website
@@ -42,37 +42,37 @@ class PHP_Widget extends WP_Widget {
     ob_end_clean();
     ?>
       <div class="execphpwidget"><?php echo $instance['filter'] ? wpautop($text) : $text; ?></div>
-                        <?php
-                        echo $after_widget;
-                      }
-                      public function form($instance) {
-                    // Erstellt das Kontroll-Formular im WP-Dashboard
-                        $instance = wp_parse_args((array) $instance, array('title' => '', 'text' => ''));
-                        $title = strip_tags($instance['title']);
-                        $text = format_to_edit($instance['text']);
-                        ?>
-    <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'php-widget'); ?></label>
+                                <?php
+                                echo $after_widget;
+                              }
+                              public function form($instance) {
+                            // Erstellt das Kontroll-Formular im WP-Dashboard
+                                $instance = wp_parse_args((array) $instance, array('title' => '', 'text' => ''));
+                                $title = strip_tags($instance['title']);
+                                $text = format_to_edit($instance['text']);
+                                ?>
+    <p><label for="<?php echo $this->get_field_id('title'); ?>"><?php _e('Title:', 'wp-h-php-widget'); ?></label>
     <input class="widefat" id="<?php echo $this->get_field_id('title'); ?>" name="<?php echo $this->get_field_name('title'); ?>" type="text" value="<?php echo esc_attr($title); ?>" /></p>
 
     <textarea class="widefat" rows="16" cols="20" id="<?php echo $this->get_field_id('text'); ?>" name="<?php echo $this->get_field_name('text'); ?>"><?php echo $text; ?></textarea>
 
-    <p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatisch Abs&auml;tze hinzuf&uuml;gen.', 'php-widget'); ?></label></p>
-                    <?php
-                  }
-                // Updating der Widget-Funktionen
-                  public function update($new_instance, $old_instance) {
-                    $instance = $old_instance;
-                    $instance['title'] = strip_tags($new_instance['title']);
-                    if (current_user_can('unfiltered_html'))
-                      $instance['text'] = $new_instance['text'];
-                    else
-                      $instance['text'] = stripslashes(wp_filter_post_kses($new_instance['text']));
-                    $instance['filter'] = isset($new_instance['filter']);
-                    return $instance;
-                  }
-                }
-                // Registrierung Widget
-                function php_widget_init() {
-                  register_widget('php_widget');
-                }
-                add_action('widgets_init', 'php_widget_init');
+    <p><input id="<?php echo $this->get_field_id('filter'); ?>" name="<?php echo $this->get_field_name('filter'); ?>" type="checkbox" <?php checked(isset($instance['filter']) ? $instance['filter'] : 0); ?> />&nbsp;<label for="<?php echo $this->get_field_id('filter'); ?>"><?php _e('Automatisch Abs&auml;tze hinzuf&uuml;gen.', 'wp-h-php-widget'); ?></label></p>
+                            <?php
+                          }
+                        // Updating der Widget-Funktionen
+                          public function update($new_instance, $old_instance) {
+                            $instance = $old_instance;
+                            $instance['title'] = strip_tags($new_instance['title']);
+                            if (current_user_can('unfiltered_html'))
+                              $instance['text'] = $new_instance['text'];
+                            else
+                              $instance['text'] = stripslashes(wp_filter_post_kses($new_instance['text']));
+                            $instance['filter'] = isset($new_instance['filter']);
+                            return $instance;
+                          }
+                        }
+                        // Registrierung Widget
+                        function php_widget_init() {
+                          register_widget('php_widget');
+                        }
+                        add_action('widgets_init', 'php_widget_init');
